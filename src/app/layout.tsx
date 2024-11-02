4fe1ebcdd0b9e72e882e51efa40fe9bd2516ec21
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2024-11-02 20:32:38 +0500

    Adding Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Todo App By Abdullah Shaikh</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
